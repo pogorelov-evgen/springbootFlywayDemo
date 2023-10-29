fd87577493adb2292a5ca955950dd86a9fca721c
PogorelovDenis <72943262+pogorelov-evgen@users.noreply.github.com>
2023-10-29 18:13:57 +0300

    Add files via upload

---
+++ src/test/java/com/example/springbootflywaydemo/SpringbootflywaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootflywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootflywaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
